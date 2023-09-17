9c57dc93baf37ac1b549f4da788f59d084b649f2
BeefonGit <beefis@byui.edu>
2023-09-16 23:28:37 -0600

    prep 2 initial

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade_input = Console.ReadLine();
+        int grade = int.Parse(grade_input);
+        if (grade >= 90)
+        {
+            string letter_g = "A";
+            Console.WriteLine($"Your grade is {letter_g}");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            string letter_g = "B";
+            Console.WriteLine($"Your grade is {letter_g}");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            string letter_g = "c";
+            Console.WriteLine($"Your grade is {letter_g}");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            string letter_g = "B";
+            Console.WriteLine($"Your grade is {letter_g}");
+        }
+        else if (grade < 60)
+        {
+            string letter_g = "F"; 
+            Console.WriteLine($"Your grade is {letter_g}");
+        }
     }
 }
