34400cf1833319ad7be7d4d70f11e7215dcd0a75
BeefonGit <beefis@byui.edu>
2023-10-11 20:03:53 -0600

    New Code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        fraction.Fraction_c();
+        Console.WriteLine(fraction.GetFractionString());
+        int top = new int;
+        top = 5;
+        fraction.Fraction_c(top);
+        Console.WriteLine(fraction.GetFractionString());
+        int bottom = new int;
+        bottom = 10;
+        fraction.Fraction_c(bottom);
+        Console.WriteLine(fraction.GetFractionString());
+        
     }
 }
